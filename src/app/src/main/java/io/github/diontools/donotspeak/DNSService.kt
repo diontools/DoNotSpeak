bfd74be93e33dd0c7420d066f97c4d5ad6c10c84
diontools <diontools@users.noreply.github.com>
2019-01-16 19:44:36 +0900

    mute

--- src/app/src/main/java/io/github/diontools/donotspeak/DNSService.kt
+++ src/app/src/main/java/io/github/diontools/donotspeak/DNSService.kt
@@ -3,12 +3,15 @@ package io.github.diontools.donotspeak
 import android.app.Notification
 import android.app.Service
 import android.content.Intent
+import android.media.AudioManager
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
-class DNSService: Service() {
+class DNSService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         throw UnsupportedOperationException("not implemented")
     }
@@ -27,6 +30,15 @@ class DNSService: Service() {
 
         this.startForeground(1, notification)
 
+        this.mute()
+
         return START_STICKY
+    }
+
+    private fun mute() {
+        var audioManager = ContextCompat.getSystemService(this, AudioManager::class.java)
+        if (audioManager != null) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+        }
     }
 }
